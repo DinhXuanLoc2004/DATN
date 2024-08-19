6874e593ffb74dc0834ce6adebc1cb86086d0221
DinhXuanLoc2004 <dinhxuanlocct@gmail.com>
2024-08-19 16:58:54 +0700

    commit file App.tsx for branch Loc/Fix/ItemCartComponent.tsx/#33

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import RatingComponent from './src/components/layouts/RatingComponent';
 function App(): React.JSX.Element {
-  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
+  return <SafeAreaView style={{flex: 1, padding: 20}}>
+<RatingComponent arr_star={[12, 5, 4 ,2 ,0]} avegare_star={4.5}/>
+  </SafeAreaView>;
 }
 
 const styles = StyleSheet.create({});
