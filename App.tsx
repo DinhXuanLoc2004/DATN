512d114c54dcb05774fdbb38f84ddc76871a031d
DinhXuanLoc2004 <dinhxuanlocct@gmail.com>
2024-08-25 23:07:58 +0700

    #46 Loc/Fix/ProductDetailScreen.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import DetailProductScreen from './src/screens/mains/stacks/DetailProductScreen';
 function App(): React.JSX.Element {
-  return <SafeAreaView style={{flex: 1}}>
-    <DetailProductScreen/>
-  </SafeAreaView>;
+  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
 }
 
 const styles = StyleSheet.create({});
